6ecfb29245a27becb0ae6088f392f16aef77e393
K4nar7 <161333682+K4nar7@users.noreply.github.com>
2025-12-25 18:10:39 +0300

    Version 2.0

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.mode === "development"
-    ? "http://localhost:5000/api"
-    : "/api",
-  withCredentials: true,
+    baseURL: import.meta.env.MODE === "development" 
+        ? "http://localhost:5000/api" 
+        : "https://e-commerce-0ro2.onrender.com/api",
+    withCredentials: true,
 });
+
 export default axiosInstance;
