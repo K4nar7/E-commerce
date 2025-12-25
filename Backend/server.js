fe2919c50a48443b0dec9e0dd051df87b1dfbc8c
K4nar7 <161333682+K4nar7@users.noreply.github.com>
2025-12-25 18:22:35 +0300

    Version 2.1

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,42 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors"; // <--- 1. IMPORT CORS
+
 import authRoutes from './routes/auth.js'; 
 import productRoutes from './routes/products.routs.js';
 import cartRoutes from './routes/cart.routs.js';
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import couponsRoutes from './routes/coupons.routs.js';
 import paymentRoutes from './routes/payment.routs.js';
 import analyticsRoutes from './routes/analytics.routs.js';
-const app = express();
 
 dotenv.config();
-const PORT =  process.env.PORT || 5000;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: process.env.CLIENT_URL, 
+  credentials: true,              
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+}));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
-app.use("/api/auth",authRoutes);
+
+app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponsRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
