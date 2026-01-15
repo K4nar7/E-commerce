a706b11fe48e6e34337b4900806ab7cbecc4e9c2
K4nar7 <161333682+K4nar7@users.noreply.github.com>
2026-01-15 22:13:37 +0300

    version 2.3

--- Backend/lib/redis.js
+++ Backend/lib/redis.js
@@ -1,14 +1,16 @@
-import { createClient } from "redis"
-import dotenv from "dotenv";
+import { Redis } from '@upstash/redis';
+import dotenv from 'dotenv';
+
 dotenv.config();
-export const redis = createClient({
-  url:process.env.UPSTASH_URL
+if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
+    throw new Error("Upstash credentials are not set in .env file.");
+}
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-redis.on("error", function(err) {
-  throw err;
+redis.get("test_connection").catch((err) => {
+    console.error("Redis Connection Failed:", err);
 });
-await redis.connect()
-await redis.set('foo','bar');
-
-// Disconnect after usage
